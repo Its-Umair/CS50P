9782b3d75f9b13c7209ba415b2fab12bfd102a8e
Umair Ali <itsumair.2021@gmail.com>
2023-11-09 19:20:48 +0500

    Update grade.py

--- grade.py
+++ grade.py
@@ -1,15 +1,15 @@
 score = int(input("Score: "))
 
-if 90 <= score <= 100:
+if score >= 90:
     print("Grade: A")
 
-elif 80 <= score <= 90:
+elif score >= 80:
     print("Grade: B")
 
-elif 70 <= score <= 80:
+elif score >= 70:
     print("Grade: C")
 
-elif 60 <= score <= 70:
+elif score >= 60:
     print("Grade: D")
 
 else:
