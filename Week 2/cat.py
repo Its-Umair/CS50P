233fc44119e0bec486ab1f6d9594eb2799f094df
Umair Ali <itsumair.2021@gmail.com>
2023-11-15 20:11:06 +0500

    Update cat.py

--- Week 2/cat.py
+++ Week 2/cat.py
@@ -1,12 +1,14 @@
 def main():
     numbers = get_number()
-    meow(3)
+    meow(numbers)
     
 def get_number():
     while True:
-        n = int(input("What's n?"))
+        n = int(input("What's n? "))
         if n > 0:
-            return n
+            break
+    return n
+
 def meow(n):
     for _ in range(n):
         print("meow")
