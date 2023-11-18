7191dc70920f6cfe66ee25ed84b17dba738d5653
Umair Ali <itsumair.2021@gmail.com>
2023-11-18 19:01:59 +0500

    Update hhogwarts.py

--- hhogwarts.py
+++ hhogwarts.py
@@ -2,5 +2,9 @@ students = [
     {"name": "Umair", "house": "Barazai", "city": "Hazro"},
     {"name": "Qaseem", "house": "Pathan Colony", "city": "Hazro"},
     {"name": "Aarish", "house": "Yaseen", "city": "Hazro"},
-    
 ]
+
+for student in students:
+    print(student["name"])
+    print(student["house"])
+    print(student["city"])
