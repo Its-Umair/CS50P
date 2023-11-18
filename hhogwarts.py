d6b384aa928f4cca90ea8208ede70c00e1747f5f
Umair Ali <itsumair.2021@gmail.com>
2023-11-18 18:56:06 +0500

    Update hhogwarts.py

---
+++ hhogwarts.py
@@ -0,0 +1,6 @@
+students = [
+    {"name": "Umair", "house": "Barazai", "city": "Hazro"},
+    {"name": "Qaseem", "house": "Pathan Colony", "city": "Hazro"},
+    {"name": "Aarish", "house": "Yaseen", "city": "Hazro"},
+    
+]
