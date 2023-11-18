21b9fb8dec88b79804007179e5e980d0384bc8b8
Umair Ali <itsumair.2021@gmail.com>
2023-11-18 19:31:17 +0500

    Update mario.py

--- Week 2/mario.py
+++ Week 2/mario.py
@@ -1,7 +1,12 @@
 def main():
-    print_row(4)
+    print_square(3)
     
-def print_row(width):
-    print("?" * width)
-        
+    
+def print_square(size):
+    for i in range(size):
+        for j in range(size):
+            print("#", end="")
+            
+        print()
+    
 main()
