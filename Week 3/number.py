6520975dbc75ab399b2764c9b1bd6901487b220c
Umair Ali <itsumair.2021@gmail.com>
2023-11-21 18:47:41 +0500

    Update number.py

--- Week 3/number.py
+++ Week 3/number.py
@@ -1,9 +1,8 @@
 while True:
     try:
         x = int(input("What's x? "))
+        break
     except ValueError:
         print("x is not a integer")
-    else:
-        break
 
 print(f"x is {x}")
