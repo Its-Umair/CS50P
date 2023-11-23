10f56bc07d4fc64ece8456b109205ae7d8753ae5
Umair Ali <itsumair.2021@gmail.com>
2023-11-23 19:44:03 +0500

    Update number.py

--- Week 3/number.py
+++ Week 3/number.py
@@ -1,4 +1,8 @@
-def det_int():
+def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
     while True:
         try:
             x = int(input("What's x? "))
@@ -7,3 +11,4 @@ def det_int():
         else:
             break
     return x
+main()
