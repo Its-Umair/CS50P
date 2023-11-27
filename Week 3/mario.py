4d35127bba3ac9c76ca4191ed652c1eb33a9831e
Umair Ali <itsumair.2021@gmail.com>
2023-11-27 20:00:57 +0500

    Create mario.py

---
+++ Week 3/mario.py
@@ -0,0 +1,10 @@
+def main():
+    height = int(input('height: '))
+    pyramid(height)
+
+def pyramid(n):
+    for i in range(n):
+        print('#' * i)
+        
+if __name__ == '__main__':
+    main()
