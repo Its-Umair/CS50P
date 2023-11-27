a5abd4c1ce45c5eb3bdfe25829b38606113c9da4
Umair Ali <itsumair.2021@gmail.com>
2023-11-27 19:51:20 +0500

    Update number.py

--- Week 3/number.py
+++ Week 3/number.py
@@ -2,10 +2,10 @@ def main():
     x = get_int()
     print(f"x is {x}")
 
-def get_int():
+def get_int(prompt):
     while True:
         try:
-            return int(input("What's x? "))
+            return int(input(prompt))
         except ValueError:
             pass
 main()
