3b72766e26c58c294bef0bb707fbfc17d1facfca
Umair Ali <itsumair.2021@gmail.com>
2023-12-24 14:55:11 +0500

    Update name.py

--- Week 4/name.py
+++ Week 4/name.py
@@ -3,6 +3,6 @@ import sys
 if len(sys.argv) < 2:
     sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
+    sys.exit("Too many arguments")
 
 print("hello, my name is ", sys.argv[1])
