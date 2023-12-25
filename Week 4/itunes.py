37dbded3404be416fcedf061c4a20469ec313a57
Umair Ali <itsumair.2021@gmail.com>
2023-12-25 16:25:25 +0500

    Update itunes.py

--- Week 4/itunes.py
+++ Week 4/itunes.py
@@ -6,4 +6,7 @@ if len(sys.argv) != 2:
     sys.exit()
     
 response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term="+ sys.argv[1])
-print(response.json())
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
