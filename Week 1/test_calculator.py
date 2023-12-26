9990ab607549c2eb330d6a8df531caa52f8acbd9
Umair Ali <itsumair.2021@gmail.com>
2023-12-26 17:11:02 +0500

    Update test_calculator.py

--- Week 1/test_calculator.py
+++ Week 1/test_calculator.py
@@ -6,10 +6,8 @@ def main():
 
 
 def test_square():
-    if square(2) != 4:
-        print("2 squared was not 4")
-    if square(3) != 9:
-        print("3 squared was not 9")
+    assert square(2) == 4
+    assert square(3) == 9
 
 
 if __name__ == "__main__":
