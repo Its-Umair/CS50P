a080290fb37b71a95ec9bd70e28e78a50cfd5e1d
Umair Ali <itsumair.2021@gmail.com>
2023-12-26 17:57:12 +0500

    Update test_calculator.py

--- Week 1/test_calculator.py
+++ Week 1/test_calculator.py
@@ -1,2 +1,8 @@
 from calculator import square
 
+def test_square():
+    assert square(2) == 4
+    assert square(3) == 9
+    assert square(-2) == 4
+    assert square(-3) == 9
+    assert square(0) == 0
