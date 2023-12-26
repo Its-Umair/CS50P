c767c734c486198950ef7240f28c40d9b875b665
Umair Ali <itsumair.2021@gmail.com>
2023-12-26 17:08:06 +0500

    Update calculator.py

--- Week 1/calculator.py
+++ Week 1/calculator.py
@@ -2,8 +2,10 @@ def main():
     x = int(input("What is x? "))
     print("x squared is ", square(x))
 
+
 def square(n):
     return n * n
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
