f50ad4ac486bedf25ec3ca320d8e82da72fa5f87
Umair Ali <itsumair.2021@gmail.com>
2023-12-26 17:48:50 +0500

    Update test_calculator.py

--- Week 1/test_calculator.py
+++ Week 1/test_calculator.py
@@ -22,6 +22,10 @@ def test_square():
         assert square(-3) == 9
     except AssertionError:
         print("-3 squared was not 9")
+    try:
+        assert square(0) == 0
+    except AssertionError:
+        print("0 squared was not 0")
 
 
 if __name__ == "__main__":
