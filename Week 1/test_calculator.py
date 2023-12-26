16e2591671eaa164e3bc4b596e126b1ae4f280c3
Umair Ali <itsumair.2021@gmail.com>
2023-12-26 17:48:05 +0500

    Update test_calculator.py

--- Week 1/test_calculator.py
+++ Week 1/test_calculator.py
@@ -1,5 +1,6 @@
 from calculator import square
 
+
 def main():
     test_square()
 
@@ -13,6 +14,14 @@ def test_square():
         assert square(3) == 9
     except AssertionError:
         print("3 squared was not 9")
+    try:
+        assert square(-2) == 4
+    except AssertionError:
+        print("-2 squared was not 4")
+    try:
+        assert square(-3) == 9
+    except AssertionError:
+        print("-3 squared was not 9")
 
 
 if __name__ == "__main__":
