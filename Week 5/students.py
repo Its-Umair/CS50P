d167da1f7952d42bf858d8ef579888d7c8b037f0
Umair Ali <itsumair.2021@gmail.com>
2024-01-01 22:02:25 +0500

    .

--- Week 5/students.py
+++ Week 5/students.py
@@ -3,4 +3,7 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
+        students.append(f"{name} house in {house}")
         
+for student in sorted(students):
+    print(student)
