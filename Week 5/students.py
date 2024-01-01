18bc12f0bd4ed6c4a51f7403c4cfe82d2977cc86
Umair Ali <itsumair.2021@gmail.com>
2024-01-01 22:09:19 +0500

    Update students.py

--- Week 5/students.py
+++ Week 5/students.py
@@ -3,9 +3,7 @@ students = []
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
-        student = {}
-        student["name"] = name
-        student["house"] = house
+        student = {"name": name, "house": house}
         students.append(student)
         
 for student in students:
