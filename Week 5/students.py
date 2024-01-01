c6fa041db656ea8a031964d3218daa37bf0a930a
Umair Ali <itsumair.2021@gmail.com>
2024-01-01 21:53:12 +0500

    Update students.py

--- Week 5/students.py
+++ Week 5/students.py
@@ -1,4 +1,4 @@
 with open("students.csv") as file:
     for line in file:
         row = line.rstrip().split(",")
-        print(f"{row[0]} is in {row[1]}")
+        print(f"{row[0]} house is in {row[1]}")
