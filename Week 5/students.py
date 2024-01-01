a4aad44575f22bcd3fbedb4b70e9b80bf30d89e9
Umair Ali <itsumair.2021@gmail.com>
2024-01-01 22:13:22 +0500

    Update students.py

--- Week 5/students.py
+++ Week 5/students.py
@@ -6,5 +6,10 @@ with open("students.csv") as file:
         student = {"name": name, "house": house}
         students.append(student)
         
-for student in students:
+        
+def get_name(student):
+    return student["name"]    
+        
+        
+for student in sorted(students, key=get_name):
     print(f"{student['name']} is in {student['house']}")
