9e60af013bacdfe40635010aeeef4fc16b1752c2
Umair Ali <itsumair.2021@gmail.com>
2024-01-02 19:02:05 +0500

    Update students.py

--- Week 5/students.py
+++ Week 5/students.py
@@ -3,10 +3,7 @@ import csv
 students = []
 
 with open("students.csv") as file:
-    for line in file:
-        name, house = line.rstrip().split(",")
-        student = {"name": name, "house": house}
-        students.append(student)
+    reader = csv.reader(file)
         
 for student in sorted(students, key=lambda student: student["name"]):
     print(f"{student['name']} is from {student['house']}")
