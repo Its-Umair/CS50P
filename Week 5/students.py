cda11bb3e43e4ca1fe3f2be32b6f7237a4ae366c
Umair Ali <itsumair.2021@gmail.com>
2024-01-03 19:08:42 +0500

    .

--- Week 5/students.py
+++ Week 5/students.py
@@ -4,5 +4,5 @@ name = input("What's your name? ")
 home = input("Where is your home? ")
 
 with open("students.csv", "a") as file:
-    writer = csv.Dictwriter(file)
+    writer = csv.DictWriter(file, fieldnames=["name", "home"])
     writer.writerow({"name": name, "home": home})
