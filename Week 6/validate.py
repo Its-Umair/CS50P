64edda699335e4755f82afbb10d255f3012b84d2
Umair Ali <itsumair.2021@gmail.com>
2024-01-04 19:03:19 +0500

    Update validate.py

--- Week 6/validate.py
+++ Week 6/validate.py
@@ -2,7 +2,7 @@ email = input("What's your email address? ").strip()
 
 username, domain = email.split("@")
 
-if username:
+if username and "." in domain:
     print("valid")
 else:
     print("invalid")
