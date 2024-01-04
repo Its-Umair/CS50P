c44fbd47a8e02fc7b32e2808d3f3b824301db8f0
Umair Ali <itsumair.2021@gmail.com>
2024-01-04 19:05:31 +0500

    Update validate.py

--- Week 6/validate.py
+++ Week 6/validate.py
@@ -2,7 +2,7 @@ email = input("What's your email address? ").strip()
 
 username, domain = email.split("@")
 
-if username and "." in domain:
+if username and domain.endswith(".edu"):
     print("valid")
 else:
     print("invalid")
