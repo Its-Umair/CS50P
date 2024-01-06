35eedd387a284df3e60d35e44aba76a957236039
Umair Ali <itsumair.2021@gmail.com>
2024-01-06 19:00:23 +0500

    Update format.py

--- Week 6/format.py
+++ Week 6/format.py
@@ -1,5 +1,5 @@
 name = input("What's your name? ").strip()
 if "," in name:
-    last, first = name.split(", ")
-    name = f"{first} {last}"
+    first, last = name.split(",")
+    name = (f"{first} {last}")
 print(f"hello, {name}")
