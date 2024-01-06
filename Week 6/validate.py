7dc65f02a26d7f741a677512aa1d2ee4176586b0
Umair Ali <itsumair.2021@gmail.com>
2024-01-06 17:44:46 +0500

    Update validate.py

--- Week 6/validate.py
+++ Week 6/validate.py
@@ -2,7 +2,7 @@ import re
 
 email = input("What's your email address? ").strip()
 
-if re.search(r"^\w+@\w+\.edu$", email.lower()):
+if re.search(r"^\w+@\w+\.edu$", email):
     print("Valid email address")
 else:
     print("Invalid email address")
