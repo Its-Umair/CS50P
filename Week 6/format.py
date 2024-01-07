87073a707b205c1f2bbbf6b5b12a380c0c8ba463
Umair Ali <itsumair.2021@gmail.com>
2024-01-07 16:03:26 +0500

    Update format.py

--- Week 6/format.py
+++ Week 6/format.py
@@ -2,7 +2,7 @@ import re
 
 
 name = input("What's your name? ").strip()
-matches = re.search(r"^(.+), (.+)$", name)
+matches = re.search(r"^(.+), ?(.+)$", name)
 if matches:
     name = matches.group(1) + " " + matches.group(2)
 print(f"hello, {name}")
