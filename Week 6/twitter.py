96ab78159ece16a2bc7811f7f5abcbc575017f85
Umair Ali <itsumair.2021@gmail.com>
2024-01-07 18:23:05 +0500

    Update twitter.py

--- Week 6/twitter.py
+++ Week 6/twitter.py
@@ -2,5 +2,5 @@ import re
 
 url = input("URL: ").strip()
 
-if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE)
+if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
     print(f"Username:", matches.group(1))
