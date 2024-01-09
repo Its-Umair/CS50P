56469b64527e5a7cfc3e5b308fd05273068ca1bc
Umair Ali <itsumair.2021@gmail.com>
2024-01-09 18:51:54 +0500

    Update student.py

--- Week 7/student.py
+++ Week 7/student.py
@@ -1,12 +1,12 @@
 def main():
-    name, house = get_student()
-    print(f"{name} from {house}")
+    student = get_student()
+    print(f"{student[0]} from {student[1]}")
 
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return name, house
+    return (name, house)
 
 
 if __name__ == "__main__":
