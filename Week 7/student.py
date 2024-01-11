593db74dbbe5158d2b90b14b8f68f1f8d289b84c
Umair Ali <itsumair.2021@gmail.com>
2024-01-11 18:35:30 +0500

    Update student.py

--- Week 7/student.py
+++ Week 7/student.py
@@ -2,6 +2,8 @@ class Student:
     def __init__(self, name, house):
         if not name:
             raise ValueError("Please provide a name")
+        if house not in ["Barazai", "Hazro", "Musa", "Behbodi"]:
+            raise ValueError("Invalid house")
         self.name = name
         self.house = house
 
