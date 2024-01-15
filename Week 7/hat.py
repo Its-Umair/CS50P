60dfb1eb369ccddda66bdfb85496dcc8c2d7bf61
Umair Ali <itsumair.2021@gmail.com>
2024-01-15 19:27:05 +0500

    Update hat.py

--- Week 7/hat.py
+++ Week 7/hat.py
@@ -1,6 +1,9 @@
 class Hat:
+    def __init__(self):
+        self.houses = ["Barazai", "Hazro", "Attock", "Kamra"]
     def sort(self, name):
         print(name, "is in", "some house")
+
 
 hat = Hat()
 hat.sort("Harry")
