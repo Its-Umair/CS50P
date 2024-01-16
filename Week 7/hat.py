f6fa70abe30a54941cea3466198c4185371e532f
Umair Ali <itsumair.2021@gmail.com>
2024-01-16 18:06:36 +0500

    Update hat.py

--- Week 7/hat.py
+++ Week 7/hat.py
@@ -2,8 +2,7 @@ import random
 
 
 class Hat:
-    def __init__(self):
-        self.houses = ["Barazai", "Hazro", "Attock", "Kamra"]
+    houses = ["Barazai", "Hazro", "Attock", "Kamra"]
 
     def sort(self, name):
         house = random.choice(self.houses)
