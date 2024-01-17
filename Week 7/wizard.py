15133d06262367865282412cf979aca3e4891b62
Umair Ali <itsumair.2021@gmail.com>
2024-01-17 19:37:47 +0500

    Update wizard.py

--- Week 7/wizard.py
+++ Week 7/wizard.py
@@ -1,3 +1,12 @@
 class Student:
     def __init__(self, name, house):
+        self.name = name
+        self.house = house
+        
+        
+    ...
+    
+
+class Professor:
+    def __init__(self, name, house):
         
