2a3c4fbc73b9238264e5ec535f05d7e17d82ef09
Umair Ali <itsumair.2021@gmail.com>
2024-01-17 19:56:37 +0500

    Update wizard.py

--- Week 7/wizard.py
+++ Week 7/wizard.py
@@ -1,5 +1,7 @@
 class Student:
     def __init__(self, name, house):
+        if not name:
+            raise ValueError("Missing name")
         self.name = name
         self.house = house
 
@@ -8,6 +10,8 @@ class Student:
 
 class Professor:
     def __init__(self, name, subject):
+        if not name:
+            raise ValueError("Missing name")
         self.name = name
         self.subject = subject
 
