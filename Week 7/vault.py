135c904146b1406b1f7c18f81e3ab35c6f3a2d78
Umair Ali <itsumair.2021@gmail.com>
2024-01-18 18:18:10 +0500

    Update vault.py

--- Week 7/vault.py
+++ Week 7/vault.py
@@ -14,9 +14,5 @@ print(potter)
 weasley = Vault(25, 50, 100)
 print(weasley)
 
-galleons = potter.galleons + weasley.galleons
-sickles = potter.sickles + weasley.sickles
-knuts = potter.knuts + weasley.knuts
-
-total = Vault(galleons, sickles, knuts)
+total = 
 print(total)
