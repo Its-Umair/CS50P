c09a8c53b5960cb0c82ddc3101b7420f147cce33
Umair Ali <itsumair.2021@gmail.com>
2024-01-19 19:53:51 +0500

    Update houses.py

---
+++ Week 8/houses.py
@@ -0,0 +1,6 @@
+students = [
+    {"name": "Umair", "house": "Barazai"},
+    {"name": "Ali Aarish", "house": "Yasin"},
+    {"name": "Qaseem", "house": "Hazro"},
+    {"name": "Basit", "house": "Hazro"},
+]
