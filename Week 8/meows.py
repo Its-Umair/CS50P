a93a15a03ec1e4ef8a0c51205c6d3bf20d1494a5
Umair Ali <itsumair.2021@gmail.com>
2024-01-22 18:53:10 +0500

    Update meows.py

--- Week 8/meows.py
+++ Week 8/meows.py
@@ -1,7 +1,7 @@
 import argparse
 
 parser = argparse.ArgumentParser(description="Meow like a cat")
-parser.add_argument("-n", help)
+parser.add_argument("-n", help="number of times to meow")
 args = parser.parse_args()
 
 for _ in range(int(args.n)):
