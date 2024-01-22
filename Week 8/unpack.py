5f5af1c0e8cd1e20f2d73cad330fa66c8a8ce696
Umair Ali <itsumair.2021@gmail.com>
2024-01-22 19:21:21 +0500

    Update unpack.py

--- Week 8/unpack.py
+++ Week 8/unpack.py
@@ -2,6 +2,6 @@ def total(galleons, sickles, knuts):
     return (galleons * 17 + sickles) * 29 + knuts
 
 
-coins = [100, 50, 25]
+coins = {"galleons": 100, "sickles": 50, "knuts": 25}
 
 print(total(galleons=100, sickles=50, knuts=25), "knuts")
