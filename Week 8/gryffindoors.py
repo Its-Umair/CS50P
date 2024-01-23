3d54f1112b25b8f709dd09b386bbfef1fb467b75
Umair Ali <itsumair.2021@gmail.com>
2024-01-23 18:16:23 +0500

    Update gryffindoors.py

--- Week 8/gryffindoors.py
+++ Week 8/gryffindoors.py
@@ -2,7 +2,7 @@ students = [
     {"name": "Umair", "house": "Barazai"},
     {"name": "Ali Aarish", "house": "Hazro"},
     {"name": "Qaseem", "house": "Hazro"},
-    {"name": "Basit", "house": "Hazro"},
+    {"name": "Abdul Basit", "house": "Hazro"},
 ]
 
 gryffindors = [student["name"] for student in students if student["house"] == "Hazro"]
