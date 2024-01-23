1aa5d146a45f0e28f35124ca125eaa1f4d67a292
Umair Ali <itsumair.2021@gmail.com>
2024-01-23 18:12:14 +0500

    Create gryffindoors.py

---
+++ Week 8/gryffindoors.py
@@ -0,0 +1,6 @@
+students = [
+    {"name": "Umair", "house": "Barazai"},
+    {"name": "Ali Aarish", "house": "Hazro"},
+    {"name": "Qaseem", "house": "Hazro"},
+    {"name": "Basit", "house": "Hazro"},
+]
