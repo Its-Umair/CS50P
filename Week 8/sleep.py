48a1bb3ace73280ffe54b1f752683e0abe06c30b
Umair Ali <itsumair.2021@gmail.com>
2024-01-24 19:20:46 +0500

    Update sleep.py

--- Week 8/sleep.py
+++ Week 8/sleep.py
@@ -1,13 +1,14 @@
 def main():
     n = int(input("What's n? "))
-    for i in range(n):
-        print(sheep(i))
+    for s in sheep(n):
+        print(s)
 
 
 def sheep(n):
     flock = []
     for i in range(n):
         flock.append("🐑" * i)
+    return flock
 
 
 if __name__ == "__main__":
