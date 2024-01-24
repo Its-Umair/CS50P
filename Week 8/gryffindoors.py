a251c17b49a8b71efc80612fb12cbc426fdba2a3
Umair Ali <itsumair.2021@gmail.com>
2024-01-24 18:46:17 +0500

    Update gryffindoors.py

--- Week 8/gryffindoors.py
+++ Week 8/gryffindoors.py
@@ -1,16 +1,3 @@
-students = [
-    {"name": "Umair", "house": "Barazai"},
-    {"name": "Ali Aarish", "house": "Hazro"},
-    {"name": "Qaseem", "house": "Hazro"},
-    {"name": "Abdul Basit", "house": "Hazro"},
-]
+students = ["Umair", "Ali", "Aarish", "Abdul Basit"]
 
-
-def is_gryffindor(s):
-    return s["house"] == "Hazro"
-
-
-gryffindors = filter(is_gryffindor, students)
-
-for gryffindor in sorted(gryffindors, key=lambda s: s["name"]):
-    print(gryffindor["name"])
+gryffindors = []
